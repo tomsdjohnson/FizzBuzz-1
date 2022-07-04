4973d3fa67e8f4d59b0d592ab03debbd915e5df1
Adhithi <ak1920@ic.ac.uk>
2022-07-04 16:32:14 +0100

    part 2 and user inputted range implemented

--- main.py
+++ main.py
@@ -9,8 +9,47 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def multiple_of(multiple, num):
+    return multiple % num == 0
+
+
+def insert_fezz(strings):
+    for i in range(0, len(strings)):
+        if strings[i][0] == 'B':
+            strings.insert(i, "Fezz")
+            return strings
+    strings.append("Fezz")
+    return strings
+
+
+def fizzbuzz(limit):
+    for i in range(1, limit + 1):
+        strings = []
+        if multiple_of(i, 3):
+            strings.append("Fizz")
+        if multiple_of(i, 5):
+            strings.append("Buzz")
+        if multiple_of(i, 7):
+            strings.append("Bang")
+        if multiple_of(i, 11):
+            strings.append("Bong")
+        if multiple_of(i, 13):
+            strings = insert_fezz(strings)
+        if multiple_of(i, 17):
+            strings = list(reversed(strings))
+        if len(strings) == 0:
+            print(i)
+        else:
+            print(''.join(strings))
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("Welcome to FizzBuzz!\n")
+    limit = input("Please enter which number you would like to stop at: ")
+    if limit.isdigit():
+        fizzbuzz(int(limit))
+    else:
+        print("Please enter an integer!")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
